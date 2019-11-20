d94fc9c1a61538ab24cc223b5394ac523c4de432
tayama <michi.chou69@gmail.com>
2019-11-20 23:48:03 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Watch></Watch>
+  </div>
+</template>
+
+<script>
+import Watch from './components/Watch.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Watch
+  }
+}
+</script>
+
+
+
+<style>
+#app {
+  font-size:64px;
+}
+</style>
---
+++ src/components/Watch.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="watch">
+        <p>{{ time }}!</p>
+        <button v-on:click="timerStart">{{ startBtn }}</button>
+        <button v-on:click="timerStop">{{ stopBtn }}</button>
+        <button v-on:click="timerReset">{{ resetBtn }}</button>
+    </div>
+</template>
+
+<script>``
+export default {
+    name: 'Watch',
+    data: function() {
+        return {
+            time: '00:00.000',
+            startBtn: 'START',
+            stopBtn: 'STOP',
+            resetBtn: 'RESET'
+        }
+    },
+    methods: {
+        timerStart: function(){
+            let oldTime = Date.now()
+            let currentTime = Date.now() - oldTime
+            console.log(currentTime)
+        },
+        timerStop: function() {
+
+        },
+        timerReset: function() {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .watch {
+        font-size: 64px;
+    }
+</style>
