2e0537e45d51b63e6ea0416a6962b122dd1c799f
tayama <michi.chou69@gmail.com>
2019-11-24 23:31:38 +0900

    各ボタンを押したときの処理まで

--- src/components/Watch.vue
+++ src/components/Watch.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="watch">
-        <p>{{ time }}!</p>
+        <p>{{ time }}</p>
         <button v-on:click="timerStart">{{ startBtn }}</button>
         <button v-on:click="timerStop">{{ stopBtn }}</button>
         <button v-on:click="timerReset">{{ resetBtn }}</button>
@@ -12,23 +12,47 @@ export default {
     name: 'Watch',
     data: function() {
         return {
-            time: '00:00.000',
+            time: '0',
+            savedTime: '0',
             startBtn: 'START',
             stopBtn: 'STOP',
-            resetBtn: 'RESET'
+            resetBtn: 'RESET',
+            started: false,
+            timerId: undefined
         }
     },
     methods: {
+        // startBtnが押されたときの処理
         timerStart: function(){
-            let oldTime = Date.now()
-            let currentTime = Date.now() - oldTime
-            console.log(currentTime)
+            console.log(this.started)
+            if (this.started === false) {
+                let startTime = Date.now()
+                console.log(startTime)
+                let log = () => {
+                    //console.log("test")      
+                    this.time = Date.now() - startTime + parseInt(this.savedTime)
+                    //this.time++
+                    console.log(this.time)
+                }
+                this.timerId = setInterval(log, 1000) //clearIntervalで使うため
+                this.started = true
+            }
+             
+              
+            //  console.log(countUp)
         },
         timerStop: function() {
-
+            clearInterval(this.timerId)
+            this.started = false
+            this.savedTime = parseInt(this.time)
+            console.log(this.savedTime)
         },
         timerReset: function() {
-
+            //console.log("hello")
+            clearInterval(this.timerId)
+            this.time = 0
+            this.savedTime = 0
+            console.log(this.time)
         }
     }
 }
